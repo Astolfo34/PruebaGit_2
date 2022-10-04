c518b8844f769729674fa4f9dd91a799b69d111c
Sebastian <jsebasdroid31@gmail.com>
2022-10-04 10:38:00 -0500

    creacion de la clase ppersona

---
+++ src/main/java/com/example/pruebagit/Main.java
@@ -0,0 +1,8 @@
+package com.example.pruebagit;
+
+public class Main {
+    public void main (String Args[]){
+        Persona p1 = new Persona("sebas","alzate",20,"344532");
+        System.out.println(p1.toString());
+    }
+}
